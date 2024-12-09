02f028f8cf1aa15f0cc8a3b931b4d4be5939a412
Myra Ilieva <159176371+miraIllieva@users.noreply.github.com>
2024-12-09 11:22:41 -0500

    Update Matrix_Modification.py

--- Matrix_Modification.py
+++ Matrix_Modification.py
@@ -1,23 +1,28 @@
 n = int(input())
 
-matrix = [[int(i) for i in input().split()]for _ in range(n)]
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
 
 while True:
     line = input().split()
     if line[0] == "END":
         break
-    command = line[0]
+    command = line[0].upper()  # Make the command case-insensitive by converting it to uppercase
 
-    row,col,valu = map(int, line[1:])
+    row, col, value = map(int, line[1:])
 
+    # Check for invalid coordinates
     if row < 0 or row >= n or col < 0 or col >= n:
-        print("Invalid cordinates")
+        print("Invalid coordinates")
         continue
-    
-    if command == "ADD":
-        matrix[row][col] += valu
-    elif command == "Subtract":
-        matrix[row][col] -= valu
-    
 
-[print(*row) for row in matrix] 
+    if command == "ADD":
+        matrix[row][col] += value
+    elif command == "SUBTRACT":
+        matrix[row][col] -= value
+    else:
+        print("Invalid command")
+        continue
+
+# Print the final matrix
+for row in matrix:
+    print(*row)
